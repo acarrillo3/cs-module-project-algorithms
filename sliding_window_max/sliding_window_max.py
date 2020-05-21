e10d7b4c666f89391abe3162d22142d86b86d883
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-05-21 10:45:17 -0400

    sliding window max for small input test passing

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,17 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    # For loop up to k times for the window
+    # loop over everything
+    maximums = []
+    for i in range(len(nums)-k+1):
+        max = nums[i]
+        for j in range(i, i + k):
+            if(j < len(nums) and max < nums[j]):
+                max = nums[j]
+        maximums.append(max)
 
-    pass
+    return maximums
 
 
 if __name__ == '__main__':
